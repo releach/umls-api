407918f9556de548c7c04e3a65d3400c00f92c03
Rachel Leach <releach@gmail.com>
2022-12-01 21:22:55 -0500

    Removed deprecated AUTH behavior. Added definitions method.

--- src/umls_api/api.py
+++ src/umls_api/api.py
@@ -1,44 +1,10 @@
 import requests
-from lxml.html import fromstring
-from cachetools import cached, TTLCache
-
-TTL_7HRS = TTLCache(maxsize=2, ttl=25200)
-
-
-class Auth:
-    def __init__(self, api_key):
-        self._api_key = api_key
-
-    @cached(TTL_7HRS)
-    def get_single_use_service_ticket(self):
-        url = 'https://utslogin.nlm.nih.gov/cas/v1/api-key'
-        headers = {
-            'Content-type': 'application/x-www-form-urlencoded',
-            'Accept': 'text/plain',
-            'User-Agent': 'python'
-        }
-        resp = requests.post(
-            url, data={'apikey': self._api_key}, headers=headers
-        )
-        resp.raise_for_status()
-        html = fromstring(resp.text)
-        ticket_granting_ticket_url = html.xpath('//form/@action')[0]
-
-        resp = requests.post(
-            ticket_granting_ticket_url,
-            data={'service': 'http://umlsks.nlm.nih.gov'},
-            headers=headers
-        )
-        resp.raise_for_status()
-        single_use_service_ticket = resp.text
-        return single_use_service_ticket
-
 
 class API:
     BASE_URL = 'https://uts-ws.nlm.nih.gov/rest'
 
     def __init__(self, *, api_key, version='current'):
-        self._auth = Auth(api_key=api_key)
+        self._api_key = api_key
         self._version = version
 
     def get_cui(self, cui):
@@ -49,8 +15,11 @@ class API:
         url = (f'{self.BASE_URL}/semantic-network/{self._version}/TUI/{tui}')
         return self._get(url=url)
 
+    def get_definition(self, cui):
+        url = f'{self.BASE_URL}/content/{self._version}/CUI/{cui}/definitions'
+        return self._get(url=url)
+
     def _get(self, url):
-        ticket = self._auth.get_single_use_service_ticket()
-        resp = requests.get(url, params={'ticket': ticket})
-        resp.raise_for_status()
+        resp = requests.get(url, params={'apiKey': self._api_key})
+        
         return resp.json()
